55f4e3c0215849c2e4d144423afbe11bbb15910f
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-11-16 17:46:36 -0700

    arithmetic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::mem;
+
+fn main()
+{
+    // arithmetic
+
+    let mut a = 2+3*4;
+    println!("{}", a);
+    a = a+1;
+    a -= 2; // a = a -2;
+
+    println!("remainder of {} / {} = {}", a, 3, (a%3));
+
+    let a_cubed = i32::pow(a,3);
+    println!("{} cubed is {}", a, a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3);
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+    println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_pi);
 }
